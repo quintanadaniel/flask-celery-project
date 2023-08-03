1f19b5b03cd46bc7568089fefa65db1ba71eba65
Daniel Quintana <quintana.danielarmando@gmail.com>
2023-08-03 17:38:39 +0100

    create Uer model add configuration environment

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-from flask import Flask
-from celery import Celery
+from project import create_app
 
-
-app = Flask(__name__)
-
-celery = Celery(
-    __name__,
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0",
-)
-@app.route('/')
-def hello():
-    return "Hello World"
-
-
-@celery.task
-def divide(x, y):
-    import time
-    time.sleep(5)
-    return x / y
+app = create_app()
---
+++ project/__init__.py
@@ -0,0 +1,38 @@
+import os
+
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from project.config import config
+
+
+# instantiatethe extensions
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app(config_name=None):
+
+    if config_name is None:
+        config_name = os.environ.get("FLASK_CONFIG", "development")
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set the config
+    app.config.from_object(config[config_name])
+
+    # set up extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # register blueprints
+    from project.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app, "db": db}
+
+    return app
